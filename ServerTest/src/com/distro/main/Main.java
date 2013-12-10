581b4ffa130fddabcc1ede6fddfa27adc2b3d4dc
frank <frank@debianserver.debianserver.com>
2013-12-10 01:34:47 -0300

    primer commit desde Eclipse

---
+++ ServerTest/src/com/distro/main/Main.java
@@ -0,0 +1,12 @@
+package com.distro.main;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello Madafakah!");
+
+	}
+
+}
